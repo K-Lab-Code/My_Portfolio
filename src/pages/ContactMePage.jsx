bd8a2cf13e081de92a77595ad7949acdaa5d5423
Kalab Smith <kalabsb@me.com>
2025-01-17 11:25:29 -0600

    updated

--- src/pages/ContactMePage.jsx
+++ src/pages/ContactMePage.jsx
@@ -1,17 +1,29 @@
 // This is a static page mocking an "About Us" section for our fake user data
 export default function ContactPage() {
-  return (    
-  <section class="page-section" id="contactMe">
-    <h2>Contact Me</h2>
-    <div class="contact-address">
-        <address>
-            <a>555-555-5555</a>
-            <a>fake@mail.com</a>
-            <a>Github</a>
-            <a>Twitter</a>
-            <a>Spotify</a>
-        </address>
-        
-    </div>
-</section>);
+    return (
+        <section class="page-section" id="contactMe">
+            <h2>Contact Me</h2>
+            <div class="contact-address">
+                <h2>Contact Us</h2>
+                <form action="/submit_form" method="post">
+                    <label for="name">Name:</label>
+                    <br />
+                    <input type="text" id="name" name="name" required />
+                    <br />
+                    <br />
+                    <label for="email">Email:</label>
+                    <br />
+                    <input type="email" id="email" name="email" required />
+                    <br />
+                    <br />
+                    <label for="message">Message:</label>
+                    <br />
+                    <textarea id="message" name="message" required></textarea>
+                    <br />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+
+            </div>
+        </section>);
 }
